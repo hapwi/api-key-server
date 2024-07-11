c048aad88dd9d96ba5fd91e60365cc88a0ecf6fa
hapwi <59853329+hapwi@users.noreply.github.com>
2024-07-10 21:59:35 -0700

    chore: Remove unnecessary code and console logs from api.js

--- api.js
+++ api.js
@@ -1,6 +1,13 @@
+const express = require("express");
+const app = express();
+require("dotenv").config(); // Load environment variables from .env
+
 app.get("/api-keys", (req, res) => {
   console.log("API_KEY:", process.env.API_KEY);
   console.log("LEADERBOARD_SHEET_ID:", process.env.LEADERBOARD_SHEET_ID);
+  console.log("ENTRIES_SHEET_ID:", process.env.ENTRIES_SHEET_ID);
+  console.log("PLAYERS_SHEET_ID:", process.env.PLAYERS_SHEET_ID);
+  console.log("FORM_SHEET_ID:", process.env.FORM_SHEET_ID);
 
   const apiKey = process.env.API_KEY;
   const leaderboardSheetId = process.env.LEADERBOARD_SHEET_ID;
@@ -16,3 +23,5 @@ app.get("/api-keys", (req, res) => {
     formSheetId,
   });
 });
+
+module.exports = app;
